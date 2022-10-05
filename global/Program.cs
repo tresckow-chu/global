cb164ebb9c7f601fd7a751883a488bdcd8b51ef2
Mike Casas <mike.casas.public@gmail.com>
2022-10-05 11:51:25 -0400

    Added config needed for the app.

--- global/Program.cs
+++ global/Program.cs
@@ -1,11 +1,39 @@
+using Blazored.LocalStorage;
 using global;
+using global.AuthProviders;
+using global.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+string baseAddress = builder.Configuration.GetValue<string>("BaseUrl");
+
+ConfigurationHelper.Initialize(builder.Configuration);
+
+var http = new HttpClient()
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+};
+
+builder.Services.AddScoped(sp => http);
+builder.Services.AddScoped<ServiceBase>();
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+
+
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 
 await builder.Build().RunAsync();
